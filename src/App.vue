cdcbf974b19229e4d456019ffb94008aec58d854
Schnee <choe@Carstens-MBP.fritz.box>
2021-06-14 23:05:47 +0200

    Icons und Beschriftung via v-for hinzugefügt

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
             <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
               <div class="container">
-                  <a class="navbar-brand" href="#page-top">Carsten Höschele</a>
+                  <a class="navbar-brand" href="#page-top">C<span class="normalLetters">arsten</span> H<span class="normalLetters">öschele</span></a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                       Menu
                       <i class="fas fa-bars ms-1"></i>
@@ -21,6 +21,7 @@
           <header class="masthead">
               <div class="container">
                   <div class="masthead-heading text-uppercase">Hi, ich bin Carsten</div>
+                  <!-- <div class="masthead-heading text-uppercase">Moin, Snow hier</div> -->
                   <div class="masthead-subheading">Software-Entwickler und Data Engineer</div>
                   <a class="btn btn-primary btn-xl text-uppercase" href="#services">Zum Portfolio</a>
               </div>
@@ -30,34 +31,11 @@
               <div class="container">
                   <div class="text-center">
                       <h2 class="section-heading text-uppercase">Über mich</h2>
-                      <h3 class="section-subheading text-muted">Ich bin ein Software-Entwickler mit einer Leidenschaft für die Erstellung intuitiver, benutzerfreundlicher Web-Erlebnisse. </h3>
+                      <h3 class="section-subheading text-muted">Ich bin Software-Entwickler mit einer Leidenschaft für die Erstellung intuitiver, benutzerfreundlicher Web-Erlebnisse. <br>
+                      Durch meine vorherige Arbeit als Data Engineer bin ich zur Erkenntnis gekommen, dass die Arbeit am Frontend mir mehr Erfüllung gibt, als die Arbeit am Backend bzw. auf Datenbankebene.</h3>
                   </div>
-                  <div class="row text-center">
-                      <div class="col-md-4">
-                          <span class="fa-stack fa-4x">
-                              <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                              <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
-                          </span>
-                          <h4 class="my-3">E-Commerce</h4>
-                          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                      </div>
-                      <div class="col-md-4">
-                          <span class="fa-stack fa-4x">
-                              <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                              <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
-                          </span>
-                          <h4 class="my-3">Responsive Design</h4>
-                          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                      </div>
-                      <div class="col-md-4">
-                          <span class="fa-stack fa-4x">
-                              <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                              <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
-                          </span>
-                          <h4 class="my-3">Web Security</h4>
-                          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                      </div>
-                  </div>
+                  <!-- über mich section mit den skills -->
+                <UeberMich/>
               </div>
           </section>
           <!-- Portfolio Grid-->
@@ -581,17 +559,19 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import UeberMich from './components/UeberMich.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UeberMich
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/UeberMich.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="row text-center">
+    <div class="col-md-4">
+        <h3 class="sprachen">
+            Sprachen
+        </h3>
+        <div class="skills">
+            <div class="my-3 skill" v-for="(skill, index) in MySkills" :key="index">
+                <i :class="skill.image"></i>
+                <br>
+                {{ skill.language }}
+            </div>
+        </div>
+    </div>
+  <!--   <div class="col-md-4">
+        <span class="fa-stack fa-4x">
+            <i class="fas fa-circle fa-stack-2x text-primary"></i>
+            <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+        </span>
+        <h4 class="my-3">Responsive Design</h4>
+        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+    </div>
+    <div class="col-md-4">
+        <span class="fa-stack fa-4x">
+            <i class="fas fa-circle fa-stack-2x text-primary"></i>
+            <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+        </span>
+        <h4 class="my-3">Web Security</h4>
+        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+    </div> -->
+</div>
+</template>
+
+<script>
+export default {
+    name: "UeberMich",
+    data() {
+        return {
+            MySkills: [
+                {
+                    language: "HTML", 
+                    image: 'fab fa-html5 fa-4x'
+                },
+                {
+                    language: "CSS", 
+                    image: 'fab fa-css3 fa-4x'
+                },
+                {
+                    language: "JavaScript", 
+                    image: 'fab fa-js-square fa-4x'
+                },
+                {
+                    language: "VueJS", 
+                    image: 'fab fa-vuejs fa-4x'
+                },
+                {
+                    language: "MSSQL", 
+                    image: 'fas fa-database fa-4x'
+                },
+                {
+                    language: "Python", 
+                    image: 'fab fa-python fa-4x'
+                }
+            ]
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.sprachen {
+    width: 80vw;
+    padding-bottom: 1.5rem;
+
+}
+
+.skills {
+
+    /* border: 1px solid blue; */
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    gap: 2rem;
+    width: 80vw;
+    margin: 0 auto;
+    line-height: 2.5rem;
+
+    .ueberMich {
+        border: 1px solid black;
+        width: 100%;
+
+        img {
+            border: 1px solid red;
+        }
+    }
+}
+</style>>
