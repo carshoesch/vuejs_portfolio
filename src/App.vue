6ee6684575fc65f8cad1ea0d9f2144f7e1b40ca7
Schnee <choe@Carstens-MBP.fritz.box>
2021-06-15 14:29:55 +0200

    Portfolio-Section wird nun automatisch befüllt

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,7 @@
               <div class="container">
                   <div class="text-center">
                       <h2 class="section-heading text-uppercase">Portfolio</h2>
-                      <h3 class="section-subheading text-muted">Eine kleine Zusammenfassung meiner bisher privaten Projekte</h3>
+                      <h3 class="section-subheading text-muted">Eine kleine Zusammenfassung meiner bisherigen Projekte</h3>
                   </div>
                   <PortfolioItems/>
               </div>
@@ -110,75 +110,11 @@
                   </ul>
               </div>
           </section>
-          <!-- Team-->
-          <section class="page-section bg-light" id="team">
-              <div class="container">
-                  <div class="text-center">
-                      <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
-                      <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                  </div>
-                  <div class="row">
-                      <div class="col-lg-4">
-                          <div class="team-member">
-                              <img class="mx-auto rounded-circle" src="assets/img/team/1.jpg" alt="..." />
-                              <h4>Parveen Anand</h4>
-                              <p class="text-muted">Lead Designer</p>
-                              <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                              <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                              <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
-                          </div>
-                      </div>
-                      <div class="col-lg-4">
-                          <div class="team-member">
-                              <img class="mx-auto rounded-circle" src="assets/img/team/2.jpg" alt="..." />
-                              <h4>Diana Petersen</h4>
-                              <p class="text-muted">Lead Marketer</p>
-                              <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                              <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                              <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
-                          </div>
-                      </div>
-                      <div class="col-lg-4">
-                          <div class="team-member">
-                              <img class="mx-auto rounded-circle" src="assets/img/team/3.jpg" alt="..." />
-                              <h4>Larry Parker</h4>
-                              <p class="text-muted">Lead Developer</p>
-                              <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                              <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                              <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="row">
-                      <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div>
-                  </div>
-              </div>
-          </section>
-          <!-- Clients-->
-          <div class="py-5">
-              <div class="container">
-                  <div class="row align-items-center">
-                      <div class="col-md-3 col-sm-6 my-3">
-                          <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="assets/img/logos/microsoft.svg" alt="..." /></a>
-                      </div>
-                      <div class="col-md-3 col-sm-6 my-3">
-                          <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="assets/img/logos/google.svg" alt="..." /></a>
-                      </div>
-                      <div class="col-md-3 col-sm-6 my-3">
-                          <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="assets/img/logos/facebook.svg" alt="..." /></a>
-                      </div>
-                      <div class="col-md-3 col-sm-6 my-3">
-                          <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="assets/img/logos/ibm.svg" alt="..." /></a>
-                      </div>
-                  </div>
-              </div>
-          </div>
           <!-- Contact-->
           <section class="page-section" id="contact">
               <div class="container">
                   <div class="text-center">
-                      <h2 class="section-heading text-uppercase">Contact Us</h2>
-                      <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                      <h2 class="section-heading text-uppercase">Kontaktiert mich</h2>
                   </div>
                   <!-- * * * * * * * * * * * * * * *-->
                   <!-- * * SB Forms Contact Form * *-->
@@ -241,11 +177,10 @@
           <footer class="footer py-4">
               <div class="container">
                   <div class="row align-items-center">
-                      <div class="col-lg-4 text-lg-start">Copyright &copy; Your Website 2021</div>
+                      <div class="col-lg-4 text-lg-start">Copyright &copy; Carsten Höschele</div>
                       <div class="col-lg-4 my-3 my-lg-0">
-                          <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                          <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                          <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                          <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-xing"></i></a>
+                          <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-github"></i></a>
                       </div>
                       <div class="col-lg-4 text-lg-end">
                           <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
--- src/components/PortfolioItems.vue
+++ src/components/PortfolioItems.vue
@@ -7,12 +7,12 @@
                         <div class="portfolio-hover">
                             <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                         </div>
-                        <img class="img-fluid" :src="obj.image" alt="..." />
+                        <img class="img-fluid" :src=obj.image alt="..." />
                     </a>
                     <div class="portfolio-caption">
                         <div class="portfolio-caption-heading">{{ obj.caption }}</div>
                         <br>
-                        <button><a :href="obj.link_git" target="_blank">Git-Repo</a></button> &nbsp
+                        <button><a :href="obj.link_git" target="_blank">Git-Repo</a></button>
                         <button><a :href="obj.link_live" target="_blank">Live-Demo</a></button>
                     </div>
                 </div>
@@ -27,33 +27,36 @@ export default {
         return {
             portfolioProject: [
                 {
-                    image: '../assets/img/clipboard_landingpage.png',
+                    image: require('../assets/img/clipboard_landingpage.png'),
                     caption: 'Clipboard Landingpage',
                     link_git: 'https://github.com/carshoesch/clipboard_landing_page',
                     link_live: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dotcom-monitor.com%2Fblog%2Fde%2F2020%2F05%2F25%2Fmonitoring-applications-written-in-vue-js%2F&psig=AOvVaw2XW5RHczMPIrmJzJO-QPWv&ust=1623838339310000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjagq2zmfECFQAAAAAdAAAAABAD'
                 },
                 {
-                    image: '../assets/img/vfr_planner.png',
+                    image: require('../assets/img/vfr_planner.png'),
                     caption: 'Terminkalender eines Rugbyvereins',
-                    link_git: 'https://github.com/carshoesch/vfr_planner_vue_firebase'
+                    link_git: 'https://github.com/carshoesch/vfr_planner_vue_firebase',
+                    link_live: 'https://vfr-planner-61ddb.web.app/'
                 },
                 {
-                    image: '../assets/img/huddle_landingpage.png',
+                    image: require('../assets/img/huddle_landingpage.png'),
                     caption: 'Huddle Landingpage',
-                    link_git: 'https://github.com/carshoesch/huddle'
+                    link_git: 'https://github.com/carshoesch/huddle',
+                    link_live: 'https://carshoesch.github.io/huddle/',
                 },
                 {
-                    image: '../assets/img/portfolio_v1.png',
+                    image: require('../assets/img/portfolio_v1.png'),
                     caption: 'Erstes Portfolio',
-                    link_git: 'https://github.com/carshoesch/portfolio_v1'
+                    link_git: 'https://github.com/carshoesch/portfolio_v1',
+                    link_live: 'https://carshoesch.github.io/portfolio_v1/',
                 },
                 {
-                    image: '..some/thing/item.png',
+                    image: require('../assets/img/vue_twotter.png'),
                     caption: 'Twotter Blogging',
                     link_git: 'https://github.com/carshoesch/twotter'
                 },
                 {
-                    image: '..some/thing/item.png',
+                    image: require('../assets/img/vue_todoList_omo.png'),
                     caption: 'Vue Todo List',
                     link_git: 'https://github.com/carshoesch/vuejs_todoList'
                 },
